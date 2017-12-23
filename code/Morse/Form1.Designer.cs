1b3c6258fbed4d0a2a70d2e3b2ba319bae318004
sebastian garcia valencia <sgarci18@eafit.edu.co>
2017-12-23 16:57:56 -0500

    code

---
+++ code/Morse/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Morse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(37, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(115, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(37, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 241);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+
+    }
+}
+
---
+++ code/Morse/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Morse
+{
+
+
+    public partial class Form1 : Form
+    {
+        Stopwatch timer = new Stopwatch();
+        Stopwatch timer2 = new Stopwatch();
+        string textoDec;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Arbol crearArbol(List<Arbol> codificacion)
+        {
+            //aqui construye el arbol
+            Arbol decodif = new Arbol();
+
+            for (int i = 0; i < codificacion.Count; i++)
+            {
+
+                string letra = codificacion[i].letra;
+
+                string codigoBin = codificacion[i].clave;
+
+                Arbol temp = decodif;
+
+                for (int j = 0; j < codigoBin.Length; j++)
+                {
+                    if (codigoBin[j] == '0')
+                    {
+                        if (temp.hijoIzq == null)
+                            temp.hijoIzq = new Arbol();
+                        temp = temp.hijoIzq;
+                    }
+                    else
+                    {
+                        if (temp.hijoDer == null)
+                            temp.hijoDer = new Arbol();
+                        temp = temp.hijoDer;
+                    }
+
+                    if (j == codigoBin.Length - 1)
+                        temp.letra = letra;
+
+                }
+                temp = decodif;
+            }
+            return decodif;
+        }
+
+        public static string decodificacion(Arbol codificacion, string texto)
+        {
+            string textDec = "";
+            Arbol reco = codificacion;
+            int k = 0;
+            while (k < texto.Length)
+            {
+                if (texto[k] == '0')
+                    reco = reco.hijoIzq;
+                else
+                    reco = reco.hijoDer;
+                if (k == texto.Length - 1)
+                {
+                    textDec = reco.letra;
+                }
+                k++;
+            }
+
+            return textDec;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+                timer.Start();
+                timer2.Stop();
+                if (timer2.ElapsedMilliseconds > 1000 && timer2.ElapsedMilliseconds < 3000)
+                {
+                    textBox1.Text += (" ");
+                    textBox2.Text += (" ");
+                }
+                else if (timer2.ElapsedMilliseconds > 3000)
+                {
+                    textBox1.Text += ("    ");
+                    textBox2.Text += ("    ");
+                }
+
+                    timer2.Reset();
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox1.Text = traducir();
+                textBox2.Text = textBox1.Text;
+            }
+            else
+            {
+                timer2.Start();
+                timer.Stop();
+
+                if (timer.ElapsedMilliseconds < 300)
+                {
+                    textBox1.Text += ("0");
+                    textBox2.Text += (".");
+                }
+                else //if (timer.ElapsedMilliseconds > 300 && timer.ElapsedMilliseconds < 1000)
+                {
+                    textBox1.Text += ("1");
+                    textBox2.Text += ("-");
+                }
+                /* else if (timer.ElapsedMilliseconds < 2000)
+                 {
+                     textoDec += traducir();
+                     textBox1.Clear();
+                 }
+                 else
+                     textBox1.Text = textoDec;*/
+                //textBox1.Text = Convert.ToString(timer.ElapsedMilliseconds);
+                timer.Reset();
+            }
+        }
+
+        private string traducir()
+        {
+            string[] textCod;
+            string texto;
+            List<Arbol> codific = new List<Arbol>();
+            try
+            {             
+                StreamReader sr = new StreamReader("morseDictionary.txt");
+                texto = sr.ReadLine();
+                while (texto != null)
+                {                 
+                    Arbol temp = new Arbol();
+                    temp.letra = texto;
+                    texto = sr.ReadLine();
+                    temp.clave = texto;
+                    codific.Add(temp);
+                    Console.WriteLine(temp.letra + " " + temp.clave);
+                    texto = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("excepcion : " + ex.Message);
+            }
+
+            Arbol arbol = crearArbol(codific);
+            textCod = textBox1.Text.Split(' ');
+
+            for (int i = 0; i < textCod.Length; i++)
+                textoDec += decodificacion(arbol, textCod[i]);
+                
+            return textoDec;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = traducir();
+        }
+    }
+
+    public class Arbol
+    {
+        public string clave;
+        public string letra;
+        public Arbol hijoIzq;
+        public Arbol hijoDer;
+
+        public Arbol()
+        {
+            hijoIzq = null;
+            hijoDer = null;
+        }
+
+
+        public virtual void colocaIzq(Arbol V, Arbol t)
+        {
+            t.hijoIzq = V;
+        }
+
+        public virtual void colocaDer(Arbol V, Arbol t)
+        {
+            t.hijoDer = V;
+        }
+
+        public virtual void preOrden(Arbol A)
+        {
+
+            if (A != null)
+            {
+                Console.WriteLine(A.letra + "	" + A.clave + " ");
+                preOrden(A.hijoIzq);
+                preOrden(A.hijoDer);
+            }
+        }
+
+    }
+}
